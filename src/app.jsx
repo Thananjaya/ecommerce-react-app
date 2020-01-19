77469647b54d75bd5876e6a460ac51316a7f063b
Thananjaya <thananjaya091993@gmail.com>
2020-01-19 11:03:35 +0530

    initializing the project

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import HomePage from "./homepage/homepage.components";
+
+const App = () => {
+  return <HomePage />;
+};
+
+export default App;
---
+++ src/homepage/homepage.components.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="menu-listing">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="sub-title">SHOT NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Womens</h1>
+          <span className="sub-title">SHOT NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Mens</h1>
+          <span className="sub-title">SHOT NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Wallets</h1>
+          <span className="sub-title">SHOT NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Sun Flares</h1>
+          <span className="sub-title">SHOT NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
