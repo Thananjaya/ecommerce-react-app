135ce5c30987493850e5e7752c60ac4ba483dfc4
thananjaya <thananjaya091993@gmail.com>
2021-07-26 22:53:45 +0530

    added redux persist for stable state management

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,18 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage  from 'redux-persist/lib/storage';
 import  userReducer  from './user/userReducer';
 import cartReducer from './cart/cartReducer';
 
-export default combineReducers({
+const persistConfiguration = {
+	key: 'root',
+	storage,
+	whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
 	user: userReducer,
 	cart: cartReducer
 });
+
+export default persistReducer(persistConfiguration, rootReducer);
