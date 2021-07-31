d43ac093ec1ca7f277aa50d101fb3627d5d95519
thananjaya <thananjaya091993@gmail.com>
2021-07-31 22:22:48 +0530

    added menu listin gitems to the stable reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,6 +3,7 @@ import { persistReducer } from 'redux-persist';
 import storage  from 'redux-persist/lib/storage';
 import  userReducer  from './user/userReducer';
 import cartReducer from './cart/cartReducer';
+import directoryReducer from './directory/directoryReducer';
 
 const persistConfiguration = {
 	key: 'root',
@@ -12,7 +13,8 @@ const persistConfiguration = {
 
 const rootReducer = combineReducers({
 	user: userReducer,
-	cart: cartReducer
+	cart: cartReducer,
+	directory: directoryReducer
 });
 
 export default persistReducer(persistConfiguration, rootReducer);
